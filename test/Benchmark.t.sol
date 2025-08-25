9721c424f25e8b274dc3deebae0c01b24f93b47c
howy <132113803+howydev@users.noreply.github.com>
2025-08-25 19:30:25 -0400

    chore: use more optimized paymaster in prod (#313)
    * chore: use more optimized paymaster in prod
    
    * chore: fmt
    
    ---------
    
    Co-authored-by: Tanishk Goyal <goyaltanishk02@gmail.com>

--- test/Benchmark.t.sol
+++ test/Benchmark.t.sol
@@ -16,7 +16,7 @@ import "./utils/interfaces/IZerodevKernel.sol";
 import "./utils/interfaces/IAlchemyModularAccount.sol";
 import "./utils/interfaces/IPimlicoPaymaster.sol";
 import "./utils/interfaces/ISafe.sol";
-import "./utils/mocks/MockPayerWithSignature.sol";
+import "./utils/mocks/MockPayerWithSignatureOptimized.sol";
 
 interface IUniswapV2Router {
     function addLiquidity(
@@ -84,7 +84,7 @@ contract BenchmarkTest is BaseTest {
     address token1;
 
     IUniswapV2Router uniswapV2Router;
-    MockPayerWithSignature appSponsor;
+    MockPayerWithSignatureOptimized appSponsor;
     DelegatedEOA appSponsorSigner;
 
     enum PaymentType {
@@ -137,10 +137,8 @@ contract BenchmarkTest is BaseTest {
         vm.deal(relayer, type(uint128).max);
 
         // Set up app sponsor for APP_SPONSOR payment type
-        appSponsor = new MockPayerWithSignature();
-        appSponsorSigner = _randomEIP7702DelegatedEOA();
-        appSponsor.setSigner(appSponsorSigner.eoa);
-        appSponsor.setApprovedOrchestrator(address(oc), true);
+        appSponsor = new MockPayerWithSignatureOptimized(address(oc));
+        appSponsor.setSigner(paymasterSigner);
         vm.deal(address(appSponsor), type(uint128).max);
     }
 
@@ -1715,7 +1713,7 @@ contract BenchmarkTest is BaseTest {
             if (_paymentType == PaymentType.APP_SPONSOR) {
                 bytes32 digest = oc.computeDigest(u);
                 bytes32 signatureDigest = appSponsor.computeSignatureDigest(digest);
-                u.paymentSignature = _sig(appSponsorSigner, signatureDigest);
+                u.paymentSignature = _eoaSig(paymasterPrivateKey, signatureDigest);
             }
 
             u.signature = _sig(delegatedEOAs[i], u);
